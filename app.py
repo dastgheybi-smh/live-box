f12b77ad014ae492f17ee0759d872344e94cecda
dastgheybi-smh <dgsmh867@gmail.com>
2025-08-14 16:04:37 +0330

    Making backend

---
+++ app.py
@@ -0,0 +1,59 @@
+from boto3 import client as client_
+from flask import Flask, request, redirect, session
+from os.path import join
+from pytml import render
+from datetime import timedelta
+
+# -- Meta Data --
+
+app = Flask(__name__)
+app.permanent_session_lifetime = timedelta(days=5)
+
+ENDPOINT = ""
+NAME = ""
+SECRET = ""
+ACCESS_TOKEN = ""
+is_authenticated = False
+client = client_(
+    "s3",
+    endpoint_url=ENDPOINT,
+    aws_access_key_id=ACCESS_TOKEN,
+    aws_secret_access_key=SECRET
+)
+
+COLORS = [
+    "#908F49",
+    "#8A221F",
+    "#6C2364",
+    "#1A1B61",
+    "#206861",
+    "#1F6817"
+]
+
+BASE_DIR = "C:\\ProgramData\\Live code"
+TEMP_DIR = join(BASE_DIR, "temp")
+
+# -- Database Settings --
+#todo: make db
+
+# -- Backend --
+
+# -- Middleware --
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render("templates/login.html")
+
+@app.route("/sign-up", methods=["GET", "POST"])
+def signup():
+    return render("templates/sign-up.html")
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if "username" in session:
+        return "You are logged in"
+    else:
+        return redirect("/login")
+
+if __name__ == "__main__":
+    app.run(debug=True)
