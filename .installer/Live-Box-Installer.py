07a20d75f10bde41d35063655ae5ea052156bf04
dastgheybi-smh <dgsmh867@gmail.com>
2025-08-14 18:19:21 +0330

    made ui

--- .installer/Live-Box-Installer.py
+++ .installer/Live-Box-Installer.py
@@ -1,1 +1,11 @@
-from colorama import 
+from colorama import Fore
+
+
+print(Fore.LIGHTWHITE_EX + f"""Welcome to {Fore.LIGHTGREEN_EX}Live Box{Fore.LIGHTWHITE_EX}
+A place to share your {Fore.LIGHTMAGENTA_EX}codes{Fore.LIGHTWHITE_EX}""")
+
+endpoint = input(f"Enter the server {Fore.LIGHTBLACK_EX}Endpoint URL{Fore.CYAN}: ")
+bucket = input(f"{Fore.LIGHTWHITE_EX}Enter the server {Fore.LIGHTBLUE_EX}Bucket Name{Fore.CYAN}: ")
+access = input(f"{Fore.LIGHTWHITE_EX}Enter the bucket {Fore.LIGHTYELLOW_EX}Access Key{Fore.CYAN}: ")
+secret = input(f"{Fore.LIGHTWHITE_EX}Enter the bucket {Fore.LIGHTRED_EX}Secret Key{Fore.CYAN}: ")
+
