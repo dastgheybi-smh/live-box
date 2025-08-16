d3e39c299e5ff58c993c7a0930ce655233e39594
dastgheybi-smh <dgsmh867@gmail.com>
2025-08-16 17:56:38 +0330

    Bug fixed

--- installer/Live-Box-Installer.py
+++ installer/Live-Box-Installer.py
@@ -6,8 +6,8 @@ A place to share your {Fore.LIGHTMAGENTA_EX}codes{Fore.LIGHTWHITE_EX}""")
 
 endpoint = input(f"Enter the server {Fore.LIGHTBLACK_EX}Endpoint URL{Fore.CYAN}: ")
 bucket = input(f"{Fore.LIGHTWHITE_EX}Enter the server {Fore.LIGHTBLUE_EX}Bucket Name{Fore.CYAN}: ")
-access = input(f"{Fore.LIGHTWHITE_EX}Enter the bucket {Fore.LIGHTYELLOW_EX}Access Key{Fore.CYAN}: ")
 secret = input(f"{Fore.LIGHTWHITE_EX}Enter the bucket {Fore.LIGHTRED_EX}Secret Key{Fore.CYAN}: ")
+access = input(f"{Fore.LIGHTWHITE_EX}Enter the bucket {Fore.LIGHTYELLOW_EX}Access Key{Fore.CYAN}: ")
 
 with open("../secret.py", "w") as f:
     f.write(
